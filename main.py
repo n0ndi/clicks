324a03bf7b533e428d82885f5d76994c3fbef978
n0ndi <92996169+n0ndi@users.noreply.github.com>
2022-07-31 10:54:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import argparse
+import os
+
 import requests
 from dotenv import load_dotenv
-import os
 from urllib.parse import urlparse
 
 
 def get_bitlink(link, bitly_token):
     headers = {
         "Authorization": f"Bearer {bitly_token}",
-              }
+        }
     url = "https://api-ssl.bitly.com/v4/bitlinks"
     long_url = {
         "long_url": link
-               }
+        }
     response = requests.post(url, json=long_url, headers=headers)
     response.raise_for_status()
     return response.json()["id"]
@@ -22,12 +23,12 @@ def count_clicks(bitlink, bitly_token):
     bitlink = urlparse(bitlink)
     headers = {
         "Authorization": f"Bearer {bitly_token}",
-              }
+        }
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink.netloc}{bitlink.path}/clicks/summary"
     params = {
         "unit": "day",
         "units": -1
-              }
+        }
     response = requests.get(url, params=params, headers=headers)
     response.raise_for_status()
     return response.json()["total_clicks"]
@@ -36,7 +37,7 @@ def count_clicks(bitlink, bitly_token):
 def is_bitlink(link, bitly_token):
     headers = {
         "Authorization": f"Bearer {bitly_token}",
-              }
+        }
     link = urlparse(link)
     link = f"{link.netloc}{link.path}"
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{link}"
@@ -49,8 +50,8 @@ def main():
     bitly_token = os.getenv("BITLY_TOKEN")
     parser = argparse.ArgumentParser()
     parser.add_argument("site_link")
-    link = parser.parse_args()
-    link = link.site_link
+    input_links = parser.parse_args()
+    link = input_links.site_link
     try:
         if is_bitlink(link, bitly_token):
             print("Количество переходов:", count_clicks(link, bitly_token))
